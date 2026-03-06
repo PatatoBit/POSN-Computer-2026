c5049004f83110c042d0f71d2d735ab58811f7f9
PatatoBit <pathonscript@gmail.com>
2026-03-06 11:17:07 +0700

    looblike complete

--- practice/programming.in.th/looblike.cpp
+++ practice/programming.in.th/looblike.cpp
@@ -1,1 +1,31 @@
 // https://programming.in.th/tasks/0033
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, max = 0;
+	cin >> n;
+
+	vector<int> dict(10001, 0);
+
+	while (n--)
+	{
+		int x;
+		cin >> x;
+
+		dict[x]++;
+
+		if (dict[x] > max)
+			max = dict[x];
+	}
+
+	for (int i = 0; i < dict.size(); i++)
+	{
+		if (dict[i] == max)
+			cout << i << " ";
+	}
+
+	return 0;
+}
